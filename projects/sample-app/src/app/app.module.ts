0156dd6b2e3ad2b1a12dc8e68c314d4cbf69d654
Zita Szupera <szuperaz@gmail.com>
2021-09-14 12:16:42 +0200

    Add sample code to sample app

--- projects/sample-app/src/app/app.module.ts
+++ projects/sample-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StreamChatAngularModule } from 'stream-chat-angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StreamChatAngularModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- projects/stream-chat-angular/src/lib/stream-chat-angular.component.ts
+++ projects/stream-chat-angular/src/lib/stream-chat-angular.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'stream-stream-chat-angular',
-  template: ` <p>stream-chat-angular works!</p> `,
+  template: ` <p>stream-chat-angular works! and reloads</p> `,
   styles: [],
 })
 export class StreamChatAngularComponent {
