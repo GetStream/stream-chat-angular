2e89fa753f8e5437514923942ffc745e4be1574b
Zita Szupera <szuperaz@gmail.com>
2021-09-29 10:12:01 +0200

    feat: Enhance scroll behavior #27
    Refactor components so the chat UI component tree is similar to React
    
    Add tests to message list
    
    Add workaround for scroll move after image load
    
    Add scroll to bottom behavior
    
    Adjust image load timeout
    
    Add docs
    
    Add compatibilty styles with React SDK
    
    Remove unnecessary code
    
    Enhance docs

--- projects/stream-chat-angular/src/lib/channel/channel.component.ts
+++ projects/stream-chat-angular/src/lib/channel/channel.component.ts
@@ -1,20 +1,10 @@
-import { Component, Input, TemplateRef } from '@angular/core';
-import { Channel } from 'stream-chat';
-import { ChannelService } from '../channel.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'stream-channel',
   templateUrl: './channel.component.html',
-  styleUrls: ['./channel.component.scss'],
+  styles: [],
 })
 export class ChannelComponent {
-  @Input() messageTemplate: TemplateRef<any> | undefined;
-  activeChannel: Channel | undefined;
-
-  constructor(private channelService: ChannelService) {
-    this.channelService.activeChannel$.subscribe(
-      (c) => (this.activeChannel = c)
-    );
-    this.activeChannel?.state.messages;
-  }
+  constructor() {}
 }
--- projects/stream-chat-angular/src/lib/mocks/index.ts
+++ projects/stream-chat-angular/src/lib/mocks/index.ts
@@ -1,5 +1,6 @@
-import { UserResponse } from 'stream-chat';
 import { DefaultUserType, StreamMessage } from '../types';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { Channel, UserResponse } from 'stream-chat';
 
 export const mockCurrentUser = () =>
   ({
@@ -18,3 +19,42 @@ export const mockMessage = () =>
     created_at: new Date('2021-09-14T13:08:30.004112Z'),
     readBy: [{ id: 'alice', name: 'Alice' }],
   } as any as StreamMessage);
+
+const generateMessages = (offset = 0, isOlder = false) => {
+  const messages = new Array(25).fill(null).map((_, index) => {
+    const message = mockMessage();
+    message.created_at = new Date(
+      message.created_at.getUTCMilliseconds() +
+        (isOlder ? -1 : 1) * (index + offset)
+    );
+    message.id = index.toString() + offset.toString();
+    return message;
+  });
+
+  return messages;
+};
+
+export type MockChannelService = {
+  activeChannelMessages$: BehaviorSubject<StreamMessage[]>;
+  activeChannel$: Subject<Channel>;
+  loadMoreMessages: () => void;
+};
+
+export const mockChannelService = (): MockChannelService => {
+  const messages = generateMessages();
+  const activeChannelMessages$ = new BehaviorSubject<StreamMessage[]>(messages);
+  const activeChannel$ = new BehaviorSubject<Channel>({
+    id: 'channelid',
+  } as Channel);
+
+  const loadMoreMessages = () => {
+    const currentMessages = activeChannelMessages$.getValue();
+    const messages = [
+      ...generateMessages(currentMessages.length, true),
+      ...currentMessages,
+    ];
+    activeChannelMessages$.next(messages);
+  };
+
+  return { activeChannelMessages$, activeChannel$, loadMoreMessages };
+};
