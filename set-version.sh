de19718030e32583d6292cf38e757ec389b1cc04
Zita Szupera <szuperaz@gmail.com>
2021-11-08 15:21:28 +0100

    chore: Fix set-version script

--- set-version.sh
+++ set-version.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-VERSION="export const version = '$1';"
+VERSION=$1
+VERSION_VAR="export const version = '$VERSION';"
 
 echo -n "" > ./projects/stream-chat-angular/src/assets/version.ts
-echo $VERSION > ./projects/stream-chat-angular/src/assets/version.ts
+echo $VERSION_VAR > ./projects/stream-chat-angular/src/assets/version.ts
+sed -i "s/\d\.\d\.\d/\$VERSION/" ./projects/stream-chat-angular/package.json
 npm run build:prod
