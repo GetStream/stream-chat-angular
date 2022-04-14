576c80f278bab80f96731cbb803f7acbb47b331f
Zita Szupera <szuperaz@gmail.com>
2022-04-14 13:45:28 +0200

    fix: Remove English words from list-users method

--- projects/stream-chat-angular/src/lib/list-users.spec.ts
+++ projects/stream-chat-angular/src/lib/list-users.spec.ts
@@ -11,17 +11,17 @@ describe('listUsers', () => {
         { id: 'id1', name: 'Sara' },
         { id: 'id2', name: 'Jane' },
       ])
-    ).toBe('Sara and Jane');
+    ).toBe('Sara, Jane');
   });
 
   it('should return correct result if user has no name, the fallback should be id', () => {
     expect(listUsers([{ id: 'id1' }, { id: 'id2', name: 'Jane' }])).toBe(
-      'id1 and Jane'
+      'id1, Jane'
     );
   });
 
   it(`should return the correct result if called with one user`, () => {
-    expect(listUsers([{ id: 'id2', name: 'Jane' }])).toBe('Jane ');
+    expect(listUsers([{ id: 'id2', name: 'Jane' }])).toBe('Jane');
   });
 
   it('should return the correct result if called with more than two users, but less or equal than five', () => {
@@ -33,7 +33,7 @@ describe('listUsers', () => {
       { id: 'id5', name: 'John' },
     ];
 
-    expect(listUsers(readBy)).toBe('Bob, Sophie, Jack, John, and Rose');
+    expect(listUsers(readBy)).toBe('Bob, Sophie, Jack, Rose, John');
   });
 
   it('should return the correct result if called with more than five users', () => {
@@ -46,6 +46,6 @@ describe('listUsers', () => {
       { id: 'id6', name: 'Adam' },
     ];
 
-    expect(listUsers(readBy)).toBe('Bob, Sophie, Jack, Rose, John and 1 more');
+    expect(listUsers(readBy)).toBe('Bob, Sophie, Jack, Rose, John +1');
   });
 });
--- projects/stream-chat-angular/src/lib/list-users.ts
+++ projects/stream-chat-angular/src/lib/list-users.ts
@@ -6,24 +6,10 @@ export const listUsers = (users: UserResponse[]) => {
   const slicedArr = users.map((item) => item.name || item.id).slice(0, 5);
   const restLength = users.length - slicedArr.length;
 
-  if (slicedArr.length === 1) {
-    outStr = `${slicedArr[0]} `;
-  } else if (slicedArr.length === 2) {
-    // joins all with "and" but =no commas
-    // example: "bob and sam"
-    outStr = `${slicedArr[0]} and ${slicedArr[1]}`;
-  } else if (slicedArr.length > 2) {
-    // joins all with commas, but last one gets ", and" (oxford comma!)
-    // example: "bob, joe, sam and 4 more"
-    if (restLength === 0) {
-      // mutate slicedArr to remove last user to display it separately
-      const lastUser = slicedArr.splice(slicedArr.length - 2, 1)[0];
-      const commaSeparatedUsers = slicedArr.join(', ');
-      outStr = `${commaSeparatedUsers}, and ${lastUser}`;
-    } else {
-      const commaSeparatedUsers = slicedArr.join(', ');
-      outStr = `${commaSeparatedUsers} and ${restLength} more`;
-    }
+  const commaSeparatedUsers = slicedArr.join(', ');
+  outStr = commaSeparatedUsers;
+  if (restLength > 0) {
+    outStr += ` +${restLength}`;
   }
 
   return outStr;
