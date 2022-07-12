468c02d562da2a1f69c039e274d7caf017d03984
Zita Szupera <szuperaz@gmail.com>
2022-07-12 16:42:02 +0200

    fix: Message component throws error if chatClient is not defined

--- projects/stream-chat-angular/src/lib/message-list/message-list.component.spec.ts
+++ projects/stream-chat-angular/src/lib/message-list/message-list.component.spec.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
 import { Channel } from 'stream-chat';
 import { AvatarPlaceholderComponent } from '../avatar-placeholder/avatar-placeholder.component';
 import { AvatarComponent } from '../avatar/avatar.component';
@@ -51,7 +52,10 @@ describe('MessageListComponent', () => {
         { provide: ChannelService, useValue: channelServiceMock },
         {
           provide: ChatClientService,
-          useValue: { chatClient: { user: mockCurrentUser() } },
+          useValue: {
+            chatClient: { user: mockCurrentUser() },
+            user$: of(mockCurrentUser()),
+          },
         },
       ],
     });
