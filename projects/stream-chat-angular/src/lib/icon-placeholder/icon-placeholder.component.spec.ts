e45f0a3dddddb9e0c044df2283abe30da51776cf
Zita Szupera <szuperaz@gmail.com>
2023-06-23 10:12:05 +0200

    perf: Refactor icon context

--- projects/stream-chat-angular/src/lib/icon-placeholder/icon-placeholder.component.spec.ts
+++ projects/stream-chat-angular/src/lib/icon-placeholder/icon-placeholder.component.spec.ts
@@ -3,6 +3,7 @@ import { By } from '@angular/platform-browser';
 import { IconComponent } from '../icon/icon.component';
 
 import { IconPlaceholderComponent } from './icon-placeholder.component';
+import { SimpleChange, SimpleChanges } from '@angular/core';
 
 describe('IconPlaceholderComponent', () => {
   let component: IconPlaceholderComponent;
@@ -27,6 +28,7 @@ describe('IconPlaceholderComponent', () => {
   it('should bind inputs', () => {
     component.icon = 'action-icon';
     component.size = 30;
+    component.ngOnChanges();
     fixture.detectChanges();
 
     const iconComponent = fixture.debugElement.query(
@@ -35,5 +37,31 @@ describe('IconPlaceholderComponent', () => {
 
     expect(iconComponent.icon).toBe('action-icon');
     expect(iconComponent.size).toBe(30);
+  });
+
+  it('should ipdate inputs', () => {
+    component.icon = 'action-icon';
+    component.size = 30;
+    fixture.detectChanges();
+
+    component.icon = 'arrow-down';
+    component.ngOnChanges();
+    fixture.detectChanges();
+
+    let iconComponent = fixture.debugElement.query(By.directive(IconComponent))
+      .componentInstance as IconComponent;
+
+    expect(iconComponent.icon).toBe('arrow-down');
+    expect(iconComponent.size).toBe(30);
+
+    component.size = 25;
+    component.ngOnChanges();
+    fixture.detectChanges();
+
+    iconComponent = fixture.debugElement.query(By.directive(IconComponent))
+      .componentInstance as IconComponent;
+
+    expect(iconComponent.icon).toBe('arrow-down');
+    expect(iconComponent.size).toBe(25);
   });
 });
