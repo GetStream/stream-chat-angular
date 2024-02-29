1d5808c798ad9604e88f0d1daf8d4a31b4bce5ea
James Finlinson <the30clues@gmail.com>
2024-02-29 22:16:43 +0000

    Add target="_blank" inside link expander which should solve the issue

--- projects/stream-chat-angular/src/lib/message/message.component.ts
+++ projects/stream-chat-angular/src/lib/message/message.component.ts
@@ -496,7 +496,8 @@ export class MessageComponent
     }
     content = content.replace(
       this.urlRegexp,
-      (match) => `<a href="${match}" rel="nofollow">${match}</a>`
+      (match) =>
+        `<a href="${match}" target="_blank" rel="nofollow">${match}</a>`
     );
 
     return content;
